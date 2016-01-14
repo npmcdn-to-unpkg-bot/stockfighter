9421d561f021a3525392a9e21bc479b9300b76b1
ashley blackmore <ashley@blackmo.re>
2016-01-13 23:57:22 -0600

    influx, rickshaw deps

--- go.py
+++ go.py
@@ -1,5 +1,9 @@
+import argparse
 import json
 import random
+import time
+
+from influxdb import InfluxDBClient
 
 import settings
 import stockfighter
@@ -8,37 +12,74 @@ import stockfighter
 def stat(text, resp):
     print(text)
     print(json.dumps(resp.json(), indent=2))
+    return resp.json()
 
 
-sf = stockfighter.Stockfighter(
-    account="AAH50221105",
-    venue="CELUEX",
-    api_key=settings.api_key,
-)
-
-stock = "HRE"
-
-sf.get_latest_quote(stock)
-sf.get_orderbook(stock)
-
-total_fills = 100000
-
-while True:
-    # dumb block-order fill
-    quote = sf.get_latest_quote(stock)
-    stat("quote", quote)
-
-    qty = random.randint(1, 50000)
-    price = random.randint(6500, 6600)
-    print("we asked for {0} at {1}".format(
-        qty,
-        price,
-    ))
-    buy_resp = sf.place_order(
-        stock=stock,
-        order_type="limit",
-        quantity=qty,
-        price=price,
-        direction="buy",
+def launch(account, venue, stock):
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'mydb')
+    sf = stockfighter.Stockfighter(
+        account=args.account,
+        venue=args.venue,
+        api_key=settings.api_key,
     )
-    stat("buy:", buy_resp)
+
+    sf.get_latest_quote(stock)
+    sf.get_orderbook(stock)
+
+    total_fills = 100000
+
+    while True:
+        quote = sf.get_latest_quote(stock).json()
+        json_body = [
+            {
+                "measurement": "quote",
+                "tags": {},
+                "time": quote["quoteTime"],
+                "fields": quote,
+            }
+        ]
+        client.write_points(json_body)
+        print(json.dumps(json_body, indent=4))
+
+        #  qty = random.randint(1, 500)
+        #  price = random.randint(3500, 3600)
+        #  print("we asked for {0} at {1}".format(
+        #      qty,
+        #      price,
+        #  ))
+        #  buy_resp = sf.place_order(
+        #      stock=stock,
+        #      order_type="limit",
+        #      quantity=qty,
+        #      price=price,
+        #      direction="buy",
+        #  )
+        #  stat("buy:", buy_resp)
+
+        #  time.sleep(1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "account",
+        help="Your brokerage account number",
+        type=str,
+    )
+    parser.add_argument(
+        "venue",
+        help="Symbol for the stock exchange to be used",
+        type=str,
+    )
+    parser.add_argument(
+        "stock",
+        help="The stock ticker symbol to be bought",
+        type=str,
+    )
+    args = parser.parse_args()
+
+    launch(
+        account=args.account,
+        venue=args.venue,
+        stock=args.stock,
+    )
